4658ea4d9e7ef1ebbff87ea64d66107d6b69f54f
BitZhouCoder <1060621144@qq.com>
2023-10-22 14:42:35 +0800

    初学

---
+++ first learn/10_11_2023/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int intput = 0;
+//	printf("周志斌爱夏芷湉\n");                       if循环
+//	printf("周志斌爱她吗(1/0)?");
+//	scanf("%d", &intput);
+//	if (intput == 1)
+//	printf("get her");
+//	else
+//		printf("lost her");
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+//int main()
+//{
+//	int line = 0;
+//	while(line < 3000000000)
+//	{
+//		printf("xie dai ma:%d\n",line);          while循环，重复一个行为直止
+//		line++;
+//	}
+//	if (line == 30000000000)
+//	{
+//		printf("hao offer\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+
+//int add(int x, int y)
+//{
+//	int z = 0;
+//	z = x * y;
+//	return z;
+//}
+//int main()                                        函数加减乘除
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = add(num1, num2);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
